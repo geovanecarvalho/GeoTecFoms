27c05252fbccbbc27b56de6de954ac1601e505b2
Geovane Carvalho <geovaneoliveira.75@hotmail.com>
2019-08-29 18:52:28 -0300

    mdlCadastro model

---
+++ Model/MdlCadastro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class MdlCadastro
+    {
+        private string data;
+        private string name;
+        private string description;
+
+        public string getData()
+        {
+            return this.data;
+        }
+        public void setData(string data)
+        {
+            this.data = data;
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getDescription()
+        {
+            return this.description;
+        }
+
+        public void setDescription(string description)
+        {
+            this.description = description;
+        }
+    }
+}
